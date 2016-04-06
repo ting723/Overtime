63de732e684224557e07798da0c62bc91a982ba1
ting723 <ting723@gmail.com>
2016-04-07 00:13:49 +0800

    完成基本的Web工程

--- src/main/java/overtime/zhanglw/www/controller/OvertimeController.java
+++ src/main/java/overtime/zhanglw/www/controller/OvertimeController.java
@@ -1,6 +1,14 @@
 package overtime.zhanglw.www.controller;
 
-public class OvertimeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-	
+@Controller
+@RequestMapping("/overtime")
+public class OvertimeController {
+	@RequestMapping(value = "/generateExcel", method = { RequestMethod.GET,RequestMethod.POST})
+	public void generateExcel() {
+		System.out.println("Hello World!");
+	}
 }
