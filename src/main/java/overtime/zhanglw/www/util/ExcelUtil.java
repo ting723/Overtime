1bb6acfddb8a6e99ad44058209483d05e36965cc
ting723 <ting723@gmail.com>
2016-04-25 19:54:32 +0800

    改善excel显示

--- src/main/java/overtime/zhanglw/www/util/ExcelUtil.java
+++ src/main/java/overtime/zhanglw/www/util/ExcelUtil.java
@@ -11,7 +11,9 @@ import org.apache.poi.ss.usermodel.DataFormat;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.extensions.XSSFCellFill;
 import org.springframework.core.io.ClassPathResource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,7 +26,7 @@ public class ExcelUtil {
 	private static String url = "http://localhost:8080/test.xlsx";
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
+	
 	public static String createExcel(String json) {
 
 		ObjectMapper mapper = new ObjectMapper();
@@ -53,37 +55,66 @@ public class ExcelUtil {
 			CellStyle dateStyle = wb.createCellStyle();
 			DataFormat dateFormat = wb.createDataFormat();
 			dateStyle.setDataFormat(dateFormat.getFormat("yyyy-MM-dd"));
-
+			dateStyle.setBorderBottom(XSSFCellStyle.BORDER_THIN); //下边框    
+			dateStyle.setBorderLeft(XSSFCellStyle.BORDER_THIN);//左边框    
+			dateStyle.setBorderTop(XSSFCellStyle.BORDER_THIN);//上边框    
+			dateStyle.setBorderRight(XSSFCellStyle.BORDER_THIN);//右边框 
+			
+			CellStyle commonStyle = wb.createCellStyle();
+			commonStyle.setBorderBottom(XSSFCellStyle.BORDER_THIN); //下边框    
+			commonStyle.setBorderLeft(XSSFCellStyle.BORDER_THIN);//左边框    
+			commonStyle.setBorderTop(XSSFCellStyle.BORDER_THIN);//上边框    
+			commonStyle.setBorderRight(XSSFCellStyle.BORDER_THIN);//右边框 
+			
 			Sheet sheet1 = wb.getSheetAt(0);
 			for (int i = 0; i < employee.getOvetTimeList().size(); i++) {
 				sheet1.shiftRows(4 + i, sheet1.getLastRowNum(), 1);
 				sheet1.createRow(4 + i);
 				OverTimeBean bean = employee.getOvetTimeList().get(i);
-				Row row = sheet1.getRow(4+i);
+				Row row = sheet1.getRow(4 + i);
 				row.createCell(0);
 				row.getCell(0).setCellValue(employee.getEmptyId());
+				row.getCell(0).setCellStyle(commonStyle);
+				
 				row.createCell(1);
 				row.getCell(1).setCellValue(employee.getName());
+				row.getCell(1).setCellStyle(commonStyle);
+				
 				row.createCell(2);
 				row.getCell(2).setCellValue("开发工程师");
+				row.getCell(2).setCellStyle(commonStyle);
+
 				row.createCell(3);
 				row.getCell(3).setCellStyle(dateStyle);
 				row.getCell(3).setCellValue(sdf.parse(bean.getDate()));
+
 				row.createCell(4);
 				row.getCell(4).setCellValue(bean.getFlag() == 0 ? Constant.WORKDAY_TYPE : Constant.OFF_DAY);
+				row.getCell(4).setCellStyle(commonStyle);
+
 				row.createCell(5);
 				row.getCell(5).setCellValue(bean.getStart());
+				row.getCell(5).setCellStyle(commonStyle);
+
 				row.createCell(6);
 				row.getCell(6).setCellValue(bean.getEnd());
+				row.getCell(6).setCellStyle(commonStyle);
+
 				row.createCell(7);
 				row.getCell(7).setCellValue("1");
+				row.getCell(7).setCellStyle(commonStyle);
+
 				row.createCell(8);
 				row.getCell(8).setCellValue("加班");
+				row.getCell(8).setCellStyle(commonStyle);
+
 				row.createCell(9);
 				row.getCell(9).setCellValue(Constant.TREATMENT);
+				row.getCell(9).setCellStyle(commonStyle);
+
 			}
 			ClassPathResource file1 = new ClassPathResource("static/overtime.js");
-			FileOutputStream fOut = new FileOutputStream(file1.getFile().getParent()+File.separator+"test.xlsx");
+			FileOutputStream fOut = new FileOutputStream(file1.getFile().getParent() + File.separator + "test.xlsx");
 			wb.write(fOut);
 			fOut.close();
 		} catch (Exception e) {
